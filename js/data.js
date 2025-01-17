82839d5f1a189b334ee1f9a827503fe0f50962bf
엄민정 <113433333+jmcgillcode@users.noreply.github.com>
2025-01-18 02:57:03 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,22 +7,22 @@ const navData = {
                 {
                     title: '注册账号',
                     url: 'https://signup.live.com',
-                    icon: 'user-plus'
+                    icon: 'outlook-plus'
                 },
                 {
                     title: '登录账号',
                     url: 'https://login.live.com',
-                    icon: 'user-login'
+                    icon: 'outlook-login'
                 },
                 {
                     title: '邮箱首页',
                     url: 'https://outlook.live.com',
-                    icon: 'lock'
+                    icon: 'outlook-home'
                 },
                 {
                     title: '绑定邮箱',
                     url: 'https://account.live.com/proofs/Add',
-                    icon: 'lock'
+                    icon: 'outlook-lock'
                 }
             ]
         },
