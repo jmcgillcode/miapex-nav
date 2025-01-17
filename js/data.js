59b703430bd93b8f8bcf7ad315a81496a0adb2a8
엄민정 <113433333+jmcgillcode@users.noreply.github.com>
2025-01-18 02:25:19 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,41 @@
-
+const navData = {
+    categories: [
+        {
+            title: 'Outlook',
+            icon: 'outlook',
+            links: [
+                {
+                    title: '注册账号',
+                    url: 'https://signup.live.com',
+                    icon: 'user-plus'
+                },
+                {
+                    title: '绑定邮箱',
+                    url: 'https://account.live.com/proofs/Add',
+                    icon: 'lock'
+                }
+            ]
+        },
+        {
+            title: '小米',
+            icon: 'mi',
+            links: [
+                {
+                    title: '内地社区',
+                    url: 'https://www.xiaomi.cn',
+                    icon: 'users'
+                },
+                {
+                    title: '账号管理',
+                    url: 'https://account.xiaomi.com/fe/service/account',
+                    icon: 'settings'
+                },
+                {
+                    title: '国际社区',
+                    url: 'https://c.mi.com/global',
+                    icon: 'globe'
+                }
+            ]
+        }
+    ]
+}
